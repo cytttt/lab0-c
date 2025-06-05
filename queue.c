6d995f2dbe509dd5615079b26774850c8c663076
Antony <yc206@rice.edu>
2025-06-05 17:01:24 +0800

    test comment again

--- queue.c
+++ queue.c
@@ -35,6 +35,7 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
     // add test anno
+    // add test anno 2
     return NULL;
 }
 
